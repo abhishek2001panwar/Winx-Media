b4914f18886dab43fd7d5bc97e768c06e83cd066
abhishek2001panwar <abhishek.panwar.cseiot.2022@miet.ac.in>
2026-02-18 22:09:23 +0530

    fix(metadata): update icon type from SVG to JPEG for compatibility

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export const metadata: Metadata = {
       },
       {
         url: "/fav.jpeg",
-        type: "image/svg+xml",
+        type: "image/jpeg",
       },
     ],
     apple: "/fav.jpeg",
